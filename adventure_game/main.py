5347c8df6c35b5e1597d86848c9105308b0a2fd8
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-07-06 10:29:47 -0500

    linked rooms together, created a linked_room method

--- adventure_game/main.py
+++ adventure_game/main.py
@@ -1,7 +1,18 @@
 from room import Room
 
+# Room Instances
 kitchen = Room('Kitchen')
-ballroom = Room('Ballroom')
-dining_hall = Room('Dining Hall')
+kitchen.set_description('A dank and dirty place, buzzing with flies.')
 
-print(kitchen.name, ballroom.name, dining_hall.name)
+ballroom = Room('Ballroom')
+ballroom.set_description('A large room with ornate golden decorations on each wall.')
+
+dining_hall = Room('Dining Hall')
+dining_hall.set_description('A vast room with a shiny wooden floor; huge candlesticks guard the entrance.')
+
+# Link rooms together
+kitchen.link_room(dining_hall, 'south')
+ballroom.link_room(dining_hall, 'east')
+dining_hall.link_room(kitchen, 'north')
+dining_hall.link_room(ballroom, 'west')
+
--- adventure_game/room.py
+++ adventure_game/room.py
@@ -2,3 +2,24 @@ class Room():
     def __init__(self, room_name):
         self.name = room_name
         self.description = None
+        self.linked_rooms = {}
+
+    def describe(self):
+        print(self.description)
+
+    def get_description(self):
+        return self.description
+
+    def get_name(self):
+        return self.name
+    
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link 
+        # for testing purposes
+        print( self.name + " linked rooms :" + repr(self.linked_rooms) )
+
+    def set_description(self, room_description):
+        self.description = room_description
+
+    def set_name(self, room_name):
+        self.name = room_name
