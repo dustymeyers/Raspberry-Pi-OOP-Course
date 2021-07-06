0549903ea748943f62acefdff55910ea452e113c
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-07-06 09:14:22 -0500

    createda few new rooms

--- adventure_game/main.py
+++ adventure_game/main.py
@@ -1,5 +1,7 @@
 from room import Room
 
 kitchen = Room('Kitchen')
+ballroom = Room('Ballroom')
+dining_hall = Room('Dining Hall')
 
-print(kitchen)
+print(kitchen.name, ballroom.name, dining_hall.name)
