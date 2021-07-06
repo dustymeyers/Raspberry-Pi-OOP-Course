528c0d4a4c5e1ddbf59fab0d53a5c2c7a65931c6
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-07-06 10:41:36 -0500

    created a get_details method to describe each room

--- adventure_game/main.py
+++ adventure_game/main.py
@@ -16,3 +16,6 @@ ballroom.link_room(dining_hall, 'east')
 dining_hall.link_room(kitchen, 'north')
 dining_hall.link_room(ballroom, 'west')
 
+dining_hall.get_details()
+kitchen.get_details()
+ballroom.get_details()
--- adventure_game/room.py
+++ adventure_game/room.py
@@ -10,13 +10,22 @@ class Room():
     def get_description(self):
         return self.description
 
+    def get_details(self):
+        print('')
+        print('The ' + self.get_name())
+        print('-------------------')
+        print(self.get_description())
+        for direction in self.linked_rooms:
+            room = self.linked_rooms[direction]
+            print('The ' + room.get_name() + ' is ' + direction)
+
     def get_name(self):
         return self.name
     
     def link_room(self, room_to_link, direction):
         self.linked_rooms[direction] = room_to_link 
         # for testing purposes
-        print( self.name + " linked rooms :" + repr(self.linked_rooms) )
+        # print( self.name + " linked rooms :" + repr(self.linked_rooms) )
 
     def set_description(self, room_description):
         self.description = room_description
